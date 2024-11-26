e1ad3edb952e9c0fc4a9de3c16fa76890d9d5d8e
HsiaoHungWang <wang@ispan.com.tw>
2024-11-26 16:10:01 +0800

    JavaScript Expression

--- src/components/SecondComponent.vue
+++ src/components/SecondComponent.vue
@@ -2,12 +2,19 @@
 import { ref } from 'vue';
 
 const isLogin = ref(false)
+const i = ref(0)
+const signin = ()=>{
+  isLogin.value = !isLogin.value
+}
 </script>
 
 <template>
  <h2>第二個組件</h2>
- <button>Click</button>
+ <button @click="signin">Toggle</button>
  <p>{{ isLogin ? "登出":"登入" }}</p>
+ <p>{{new Date().toLocaleDateString()}}</p>
+ <p>{{ i+1 }}</p> <!-- i = i + 1-->
+
 </template>
 
 <style lang="css" scoped>
