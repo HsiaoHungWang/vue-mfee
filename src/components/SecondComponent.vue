fbb3f58701e18e858546714db37c6416fc83aab6
HsiaoHungWang <wang@ispan.com.tw>
2024-11-26 15:53:25 +0800

    Text Interpolation

--- src/components/SecondComponent.vue
+++ src/components/SecondComponent.vue
@@ -1,9 +1,13 @@
 <script setup>
-  
+import { ref } from 'vue';
+
+const isLogin = ref(false)
 </script>
 
 <template>
  <h2>第二個組件</h2>
+ <button>Click</button>
+ <p>{{ isLogin ? "登出":"登入" }}</p>
 </template>
 
 <style lang="css" scoped>
