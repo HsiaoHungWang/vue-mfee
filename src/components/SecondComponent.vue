05539ffc74933d4e2ac9d7ad79854a9004e0051d
HsiaoHungWang <wang@ispan.com.tw>
2024-11-26 16:36:33 +0800

    屬性繫節v-bind

--- src/components/SecondComponent.vue
+++ src/components/SecondComponent.vue
@@ -16,6 +16,8 @@ const author = ref({
     ]
 })
 
+const id = ref("100903")
+const homeUrl = ref("https://www.ispan.com.tw")
 </script>
 
 <template>
@@ -26,6 +28,15 @@ const author = ref({
  <p>{{ i+1 }}</p> <!-- i = i + 1-->
 <p>星期 {{ week[new Date().getDay()]}}</p>
 <p>作者 {{ author.name }} {{author.books.length > 0 ? "有":"還沒有"}}出版書籍</p>
+<!--屬性繫節-->
+<p :data-id="id">{{ id }}</p>
+<p :id="id">{{ id }}</p>
+<p :id>{{ id }}</p>
+<a :href="homeUrl">iSpan</a>
+
+<img src="/images/batch.jpg" />
+<img src="@/assets/anya-300-03.png" />
+
 </template>
 
 <style lang="css" scoped>
