520a2fc7035d236f3d7a1725acbf7fac429a19a3
HsiaoHungWang <wang@ispan.com.tw>
2024-11-26 15:07:21 +0800

    ref() 的說明

--- src/components/FirstComponent.vue
+++ src/components/FirstComponent.vue
@@ -1,10 +1,18 @@
 <template>
   <h2>{{ message }}</h2>
   <input type="text" v-model="message" />
+  <button @click="add">{{ count }}</button>
 </template>
 <script setup>
 import { ref } from 'vue';
 const message = ref("First Component")
+const count = ref(0)
+
+const add = ()=>{
+// console.log(count)
+// console.log(count.value)
+count.value++
+}
 
 </script>
 <style scoped>
