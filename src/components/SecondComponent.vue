d442aca5a58b35c59cebb1b055a773b93da968e8
HsiaoHungWang <wang@ispan.com.tw>
2024-11-26 16:17:14 +0800

    PPT50-51的練習

--- src/components/SecondComponent.vue
+++ src/components/SecondComponent.vue
@@ -6,6 +6,16 @@ const i = ref(0)
 const signin = ()=>{
   isLogin.value = !isLogin.value
 }
+const week = ref(["日", "一", "二", "三", "四", "五", "六"])
+const author = ref({ 
+    "name":"Jack",
+    "books": [
+        'Vue 2 - Advanced Guide',
+        'Vue 3 - Basic Guide',
+        'Vue 4 - The Mystery'
+    ]
+})
+
 </script>
 
 <template>
@@ -14,7 +24,8 @@ const signin = ()=>{
  <p>{{ isLogin ? "登出":"登入" }}</p>
  <p>{{new Date().toLocaleDateString()}}</p>
  <p>{{ i+1 }}</p> <!-- i = i + 1-->
-
+<p>星期 {{ week[new Date().getDay()]}}</p>
+<p>作者 {{ author.name }} {{author.books.length > 0 ? "有":"還沒有"}}出版書籍</p>
 </template>
 
 <style lang="css" scoped>
